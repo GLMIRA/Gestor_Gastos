28c2aa3589bd038eac36d6ba97b5a80c17eab8b9
Eric Borges <borgeseric391@gmail.com>
2025-04-16 18:13:44 -0300

    Renomei o arquivo constantes.hpp

---
+++ app/front_end/front_end.cpp
@@ -0,0 +1,13 @@
+//Bibliotecas
+#include <stdio.h>
+
+//Modulos
+#include "dados/estructs_dados.hpp"
+#include "constantes.hpp"
+
+//Codigo☺
+void teste(){
+    User user[NUM_MAX_USER]; //Numero de usuarios por meio de um vetor
+    
+
+}
