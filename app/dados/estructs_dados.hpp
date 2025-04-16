7ebd283ebea80cd4b8856f8a168450d9ff393471
Gabriel Mira <gllimamira@gamil.com>
2025-04-16 17:16:40 -0300

    estrutura de dados inicial

--- app/dados/estructs_dados.hpp
+++ app/dados/estructs_dados.hpp
@@ -1,17 +1,16 @@
 #ifndef ESTRUTURA_DADOS_HPP
 #define ESTURUTRA_DADOS_HPP
 
-struct User
-{
+struct User{
     char name[50];
     char cpf[11];
     int age = 0;
     float salary = 0.0;
 };
 
-struct Expense {
+struct Expense{
     bool typeExpense; 
-    char nameExpene[50];
+    char nameExpense[50];
     float amount = 0.0;
     
 };
