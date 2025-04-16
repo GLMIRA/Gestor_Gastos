faa7298eccdcfeccd4e1c05e1b2a20828e8e564f
Eric Borges <borgeseric391@gmail.com>
2025-04-16 20:57:40 -0300

    Adicionando os codigos da funcao menu

--- app/front_end/front_end.cpp
+++ app/front_end/front_end.cpp
@@ -6,8 +6,31 @@
 #include "constantes.hpp"
 
 //Codigo☺
-void teste(){
-    User user[NUM_MAX_USER]; //Numero de usuarios por meio de um vetor
-    
+void menu(){
 
+    //variaveis
+    char option;
+
+    printf("");//aqui vai ficar a apresentação de tela
+
+    printf("");//aqui ira ficar a mensagem "DIGITE UM VALOR" -> obs: a mensagem so serve para dar um noção do que fazer depois 
+
+    scanf("%c",&option);
+
+    switch (option)
+    {
+    case 'C':
+        /*aqui ira ficar o codigo quando o usuario for criar uma nova conta*/
+        break;
+    
+    case 'L':
+        /*aqui ira ficar o codigo quando o usuario for logar na sua conta*/
+        break;
+
+        
+
+    default:
+    break;
+}
+printf("\n|**************|\n| Wrong value! |\n**************\n");☺☻↨
 }
