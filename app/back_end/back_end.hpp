64e215345256b0646f3aac4abd8719c65bd0bafe
Gabriel Mira <gllimamira@gamil.com>
2025-04-29 18:18:51 -0300

    alteraçoes sobre funcionamento de codigo

--- app/back_end/back_end.hpp
+++ app/back_end/back_end.hpp
@@ -8,4 +8,9 @@ using namespace std;
 int validate_user(const string &cpf_formatted, const string &first_name,
     const string &last_name, const string &age_user, const string &salary);
 
+bool validate_cpf(const string &cpf_formatado);
+bool validate_name(const string &name_user, const string &last_name);
+bool validate_age(const string &age_user);
+bool validate_salary(const string &salary_user);
+
 #endif
--- app/dados/dados.cpp
+++ app/dados/dados.cpp
@@ -3,8 +3,26 @@
 #include <cctype>
 #include <string>
 
+#include "dados/structs_dados.hpp"
+
 using namespace std;
 
-void edit_type_name(){
+
+int change_string_to_int(const string &age_user){
+    int age = 0;
+    age = stoi(age_user);
+    return age;
+}
+
+float change_string_to_float(const string &salary_user){
+    float salary = 0.0;
+    salary = stof(salary_user);
+    return salary;
+}
+
+bool add_user_in_struct(const string &cpf_formatted, const string &first_name,
+const string &last_name, const string &age_user, const string &salary){
+    
+    return true;
 
 }
