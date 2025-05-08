26ba5b749fd99eb8e02d179950ade7ffe80c4406
Lucas Fares <159576387+LucasFaresC@users.noreply.github.com>
2025-05-07 22:16:31 -0300

    Feature/refatorando estruturas (#27)
    * adicionando todo no frontend
    
    * adicionando TODO no front ende refazendo estruturas
    
    * adicionando variaveis aos structs

--- app/dados/structs_dados.hpp
+++ app/dados/structs_dados.hpp
@@ -6,35 +6,36 @@
 #include <chrono>
 #include "constantes.hpp"
 
-
 using namespace std;
+
 struct Expense{
     int id;
     bool typeExpense; // se é obrigatoria ou nao (sendo TRUE obrigatoria e FALSE nao obrigatoria) 
-    char nameExpense[50]; // nome exp: LUZ,AGUA,NET 
-    float amount = 0.0;// valor da conta 
-    char dateTime[19];// recebe em formato:AAAA-MM-DD HH:MM:SS 
-    char description[100]; // texto sobre o gasto, ou breve comentário -> Atributo opcional
-    // TODO : depois de adicionar essa linha eu vou ler a documentação git e gitflow
+    string nameExpense; // nome exp: LUZ,AGUA,NET 
+    float amount = 0.0;// valor da gasto
+    char dateTime[19];// recebe em formato:AAAA-MM-DD HH:MM:SS
+    string description; // texto sobre o gasto, ou breve comentário -> Atributo opcional
 };
 
-struct ExtraSourceIncome{ // atributo opcional em user
-    int id;
+struct SourceIncome{ // atributo opcional em user
+    int id; // PK
     string name; // nome da renda -> EX: Investimentos 
     bool salary; // verificar se é salário de outro emprego, ou não -> TRUE se sim, FALSE se não
     float amount_monthly; // Quantidade de reais que essa renda traz mensalmente
+    string description; // breve comentário -> Atributo opcional
 };
 
 struct User{
-    int id;
+    int id; // PK
     string name;
     string last_name; 
+    float total_amount; // montante da renda + salario mensalmente
     char cpf[14];
-    int age; // deve receber com base na DATA DE NASCIMENTO
-    float total_amount; // montante da renda + salario
-    Expense expense[NUM_MAX_EXPENSE];
-    ExtraSourceIncome source[NUM_MAX_SOURCESOFINCOME];
-    int total_expenses = 0; // numero de gastos do usuário mensalmente
+    char birth_date[10]; // data de nascimento -> DD-MM-AAAA
+    int total_expenses = 0; // numero de gastos do usuário mensalmente -> incrementado  
+    float total_amount_expenses; // deve ser a soma de todos os expense.amount
+    Expense expense[NUM_MAX_EXPENSE]; // outra table
+    SourceIncome source[NUM_MAX_SOURCESOFINCOME]; // idem
 };
-
+//TODO: Fazer workbench
 #endif
