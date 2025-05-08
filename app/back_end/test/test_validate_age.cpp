b1e980dccff0421969575161e165c8d471713286
Gabriel Mira <gllimamira@gamil.com>
2025-05-08 00:34:53 -0300

    feat: refazendo a feature para validar idade

--- app/back_end/test/test_validate_age.cpp
+++ app/back_end/test/test_validate_age.cpp
@@ -4,12 +4,17 @@
 
 //TODO: REFATORAR OS TESTES, para que aceitem a função
 TEST(validateAge, validAge){
-    EXPECT_TRUE(validate_age("102"));
-    EXPECT_TRUE(validate_age("20"));
+    EXPECT_TRUE(validate_age("10/12/2000"));
+    EXPECT_TRUE(validate_age("01/01/1901"));
 }
 
 TEST(validadteAge, invalidAge){
     EXPECT_FALSE(validate_age("letras"));
     EXPECT_FALSE(validate_age(",.;~´'][12]"));
     EXPECT_FALSE(validate_age(" ;. /?!@#$567890"));
+    EXPECT_FALSE(validate_age("12"));
+    EXPECT_FALSE(validate_age("01/01/2015"));
+    EXPECT_FALSE(validate_age("01/01/2035"));
+    EXPECT_FALSE(validate_age("1/1/2035"));
 }
+
