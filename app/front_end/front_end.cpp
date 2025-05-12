7cf66d15a26e4e3d1c0beb14cc84f79872de8ebd
Gabriel Mira <89841536+GLMIRA@users.noreply.github.com>
2025-05-12 10:53:42 -0300

    Feature/edit funcao validate user (#34)
    * feat: refazendo a feature para validar idade
    
    * feat: refatora função de validação de idade para usar data de nascimento
    
    * feat: refatorada a função que valida o nome e sobrenome do usuario
    
    * feat: refatorando os testes para testar corretamente o nome
    
    * feat: adicionando a biblioteca uic pra validar caracteres no formato UTF-8
    
    * feat: renomemi os testetes destes modulos pra melhor indentificação
    
    * feat: edita um array de inteiros para modificar os elemntos desse array
    
    * bug:comentando para resolver o b.o. de builid
    
    ---------
    
    Co-authored-by: Gabriel Mira <gllimamira@gamil.com>

--- app/front_end/front_end.cpp
+++ app/front_end/front_end.cpp
@@ -27,71 +27,71 @@ void menu(){
     case 'c':
     case 'C':
 
-        cout << "\nBem Vindo!\n";
+        // cout << "\nBem Vindo!\n";
 
-        //Aqui sera o lugar onde o Usuario ira digitar as suas informações
+        // //Aqui sera o lugar onde o Usuario ira digitar as suas informações
 
-        cout <<"Digite o CPF: ";
-        getline(cin,cpf);
+        // cout <<"Digite o CPF: ";
+        // getline(cin,cpf);
 
-        cout <<"Digite o Primeiro Nome: ";
-        getline(cin,fist_name);
+        // cout <<"Digite o Primeiro Nome: ";
+        // getline(cin,fist_name);
 
-        cout <<"Digite o Sobrenome: ";
-        getline(cin,last_name);
+        // cout <<"Digite o Sobrenome: ";
+        // getline(cin,last_name);
 
-        /*TODO: mudança de planos, o usuario n pode ter menos que 14 anos e vai inserir a data
-        nascimento no formato dd/mm/aaaa(back-end espera receber dessa forma blzz ??)
-        vou retornar dois possiveis erros 
-        data_nacimento invalida ou usuario com idadde menor que a permitida blzz??
-        */
-        cout <<"Digite a Idade: ";
-        getline(cin,age);
+        // /*TODO: mudança de planos, o usuario n pode ter menos que 14 anos e vai inserir a data
+        // nascimento no formato dd/mm/aaaa(back-end espera receber dessa forma blzz ??)
+        // vou retornar dois possiveis erros 
+        // data_nacimento invalida ou usuario com idadde menor que a permitida blzz??
+        // */
+        // cout <<"Digite a Idade: ";
+        // getline(cin,age);
 
-        cout <<"Digite o Salario: ";
-        getline(cin,salary);
+        // cout <<"Digite o Salario: ";
+        // getline(cin,salary);
 
-        //codigo que envia as informações do usuario para a funcao validate_user
-        response = validate_user(cpf,fist_name,last_name,age,salary);
+        // //codigo que envia as informações do usuario para a funcao validate_user
+        // response = validate_user(cpf,fist_name,last_name,age,salary);
 
 
-        switch (response)
-        {
-        case 1:
-            menssager = "CPF invalido ou fora do padrao xxx.xxx.xxx-xx";
-        break;
+        // switch (response)
+        // {
+        // case 1:
+        //     menssager = "CPF invalido ou fora do padrao xxx.xxx.xxx-xx";
+        // break;
 
-        case 2:
-            menssager = "Nome tem numeros ou caracteres especiais";
-        break;
+        // case 2:
+        //     menssager = "Nome tem numeros ou caracteres especiais";
+        // break;
 
-        case 3:
-            menssager = "Idade tem letras o caracteres especiais ";
-        break;
+        // case 3:
+        //     menssager = "Idade tem letras o caracteres especiais ";
+        // break;
 
-        case 4:
-            menssager = "Salario tem letras, caracteres especiais ou nao esta no formato 0000,00";
-        break;
-        }
+        // case 4:
+        //     menssager = "Salario tem letras, caracteres especiais ou nao esta no formato 0000,00";
+//         break;
+//         }
 
-        if(response>0){
-            cout << "\n!ERRO: " <<menssager << " !\n";
-        }else{
-            cout << "\n Usuario Criado com Sucesso!\n";
-        }
+//         if(response>0){
+//             cout << "\n!ERRO: " <<menssager << " !\n";
+//         }else{
+//             cout << "\n Usuario Criado com Sucesso!\n";
+//         }
+    break;
+
+//     case 'l':
+//     case 'L':
+
+//     break;
+
         
-    break;
 
-    case 'l':
-    case 'L':
+//     default:
+//     printf("\n|**************|\n| Wrong value! |\n**************\n");   
+//     break;
+     }
 
-    break;
-
-        
-
-    default:
-    printf("\n|**************|\n| Wrong value! |\n**************\n");   
-    break;
-    }
-
+// 
 }
